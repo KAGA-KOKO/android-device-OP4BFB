f5523080d55c03730fa6db7cd2c13eaefe269b44
KAGA-KOKO <kotakandis185@gmail.com>
2024-07-07 05:16:58 +0700

    DEVICE TREE

--- lineage_OP4BFB.mk
+++ lineage_OP4BFB.mk
@@ -14,14 +14,17 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from OP4BFB device
 $(call inherit-product, device/oppo/OP4BFB/device.mk)
 
+PRODUCT_BRAND := oppo
 PRODUCT_DEVICE := OP4BFB
-PRODUCT_NAME := lineage_OP4BFB
 PRODUCT_MANUFACTURER := oppo
+PRODUCT_NAME := lineage_OP4BFB
+PRODUCT_MODEL := OPPO A12
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
+TARGET_VENDOR := oppo
+TARGET_VENDOR_PRODUCT_NAME := OP4BFB
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="full_oppo6765_19451-user 9 PPR1.180610.011 eng.root.20220118.230000 release-keys"
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="full_oppo6765_19451-user 9 PPR1.180610.011 eng.root.20220118.230000 release-keys"
-    
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := OPPO/CPH2083/OP4BFB:9/PPR1.180610.011/1572000930:user/release-keys
+
